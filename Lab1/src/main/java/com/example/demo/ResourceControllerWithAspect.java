419e63640db78ef22d5ec1d148c21abd64345caf
salmasherif846 <salmasherif846@gmail.com>
2025-03-04 01:01:49 +0200

    Init project

---
+++ Lab1/src/main/java/com/example/demo/ResourceControllerWithAspect.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/resource")
+public class ResourceControllerWithAspect {
+
+    @GetMapping
+    public String getResource() {
+        return "GET request received";
+    }
+
+    @PostMapping
+    public String createResource() {
+        return "POST request received";
+    }
+
+    @PutMapping
+    public String updateResource() {
+        return "PUT request received";
+    }
+
+    @DeleteMapping
+    public String deleteResource() {
+        return "DELETE request received";
+    }
+
+    @Aspect
+    @Component
+    public static class LoggingAspect {
+
+        @Before("execution(* com.example.demo.ResourceControllerWithAspect.*(..))")
+        public void logBeforeMethod() {
+            System.out.println("Aspect Advice: A controller method is about to be called");
+        }
+    }
+}
